aa874e0f65f5fa07bd51f013095f4922e3633336
syadav <shivanshu.yadav@amdocs.com>
2021-03-16 17:52:18 +0530

    initial

---
+++ precache-manifest.5dd7dd4904fd928f155efd6a878e7f5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a3fb6d0eabd37533094",
+    "url": "/static/css/main.760a0197.chunk.css"
+  },
+  {
+    "revision": "8a3fb6d0eabd37533094",
+    "url": "/static/js/main.8a3fb6d0.chunk.js"
+  },
+  {
+    "revision": "e3a89ca19d3b4855b0bd",
+    "url": "/static/js/1.e3a89ca1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f18dbef9c5935bce1c3b853164ffc371",
+    "url": "/index.html"
+  }
+];
